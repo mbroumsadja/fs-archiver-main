2524fc0892780fc2517368d07466545b566f110b
= <=>
2026-04-17 04:52:23 +0100

    fix color

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#004488" />
+      <meta name="theme-color" content="#001224" />
       <link rel="manifest" href="/manifest.json" />
       <link rel="apple-touch-icon" href="/icon-192.png" />
       <link rel="icon" href="/icon-192.png" sizes="192x192" type="image/png" />
